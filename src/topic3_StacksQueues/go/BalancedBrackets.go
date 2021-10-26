011c132bd5f679779a4f1fde60857b5032618cee
oscar-defelice <oscar.defelice@gmail.com>
2021-10-26 18:52:37 +0200

    solutions: Topic Stack and Queues added

---
+++ src/topic3_StacksQueues/go/BalancedBrackets.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'isBalanced' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+func isBalanced(s string) string {
+    var result string
+    n:=-1
+    for ok := true; ok; ok = (len(s)!=n) {
+        n = len(s)
+        s = strings.Replace(s, "()", "", -1)
+        s = strings.Replace(s, "[]", "", -1)
+        s = strings.Replace(s, "{}", "", -1)
+    }
+    if len(s) == 0 {
+        result = "YES"
+    } else {
+        result = "NO"
+    }
+
+    return result
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    tTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    t := int32(tTemp)
+
+    for tItr := 0; tItr < int(t); tItr++ {
+        s := readLine(reader)
+
+        result := isBalanced(s)
+
+        fmt.Fprintf(writer, "%s\n", result)
+    }
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
