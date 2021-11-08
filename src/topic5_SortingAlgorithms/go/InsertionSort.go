37d5a597031f359d415460efdc3090fc772a4e85
oscar-defelice <oscar.defelice@gmail.com>
2021-11-08 16:07:46 +0100

    solution: Added insertion sort in golang

---
+++ src/topic5_SortingAlgorithms/go/InsertionSort.go
@@ -0,0 +1,30 @@
+// QuickSort.go
+// description: Implementation of in-place quicksort algorithm
+// details:
+// A simple in-place insertion sort algorithm implementation. [Wikipedia](https://en.wikipedia.org/wiki/Quicksort)
+// author [Oscar de Felice](https://github.com/oscar-defelice)
+
+package main
+
+import (
+  "fmt"
+)
+
+// InsertionSort sorts the entire array
+func InsertionSort(arr []int) (result []int) {
+  for i := 1; i < len(arr); i++ {
+		temporary := arr[i]
+    j := i
+		for ; j > 0 && arr[j-1] >= temporary; j-- {
+			arr[j] = arr[j-1]
+		}
+		arr[j] = temporary
+	}
+	return arr
+}
+
+func main() {
+  slice := []int{5, 8, 1, 3, 7, 9, 2}
+  fmt.Println("\n--- Unsorted --- \n\n", slice)
+  fmt.Println("\n--- Sorted --- \n\n", InsertionSort(slice))
+}
